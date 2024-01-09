07b8417980bd5799e1bd8888878e26ee9e6606a9
Gaspard Delvaux <82412381+gasparddelvaux@users.noreply.github.com>
2024-01-09 18:06:09 +0100

    Details, genres and favorites

--- js/main.js
+++ js/main.js
@@ -35,6 +35,17 @@ const tvshows_display = document.getElementById('tvshows_display');
 const favorites_display = document.getElementById('favorites_display');
 const search_display = document.getElementById('search_display');
 
+// GENRES
+
+const genres_movies = document.getElementById('genres_movies');
+const genres_tvshows = document.getElementById('genres_tvshows');
+
+// MODAL
+
+const details_modal = document.getElementById('details_modal');
+const modal_content = document.getElementById('modal_content');
+const modal_close_btn = document.getElementById('modal_close_btn');
+
 // MISCELLANEOUS
 
 const add_to_favorites = '<i class="ti ti-heart-plus"> </i>Ajouter aux favoris';
@@ -44,7 +55,9 @@ const remove_to_favorites = '<i class="ti ti-heart-minus"> </i>Retirer des favor
 // MENU
 // 
 
+let active_tab = 'movies';
 function tabChange(type) {
+    active_tab = type;
     const types = ['movies', 'tvshows', 'favorites', 'search'];
     const navs = [nav_movies, nav_tvshows, nav_favorites];
     const sections = [movies_section, tvshows_section, favorites_section, search_section];
@@ -70,6 +83,7 @@ nav_tvshows.addEventListener('click', () => {
 
 nav_favorites.addEventListener('click', () => {
     tabChange('favorites');
+    loadFavorites();
 })
 
 // SEARCH
@@ -177,31 +191,33 @@ search_input.addEventListener('keyup', function (e) {
 // GET FUNCTIONS
 
 function getAllPopular(genre = null, type = null) {
+    let movies_url = 'https://api.themoviedb.org/3/discover/movie?language=fr';
+    let tvshows_url = 'https://api.themoviedb.org/3/discover/tv?language=fr';
     if(genre != null) {
-        if(type === 'movies') {
-            console.log('genre movies');
-        } else if(type === 'tvshows') {
-            console.log('genre tvshows');
+        if(type === 'movie') {
+            movies_url += `&with_genres=${genre}`;
+        } else if(type === 'tvshow') {
+            tvshows_url += `&with_genres=${genre}`;
         }
-    }else{
-        fetch('https://api.themoviedb.org/3/movie/popular?language=fr', options)
-        .then(response => response.json())
-        .then(data => {
-            const movies = data.results;
-            let output = '';
-            let fav_btn_content = '';
-            for(let movie of movies) {
-                console.log(movie.title);
-                fav_btn_content = checkFavorite(movie.id, 'movie');
-                if (movie.overview.length > 350) {
-                    movie.overview = movie.overview.substring(0,350) + "...";
-                }
-                output += `
+    }
+    fetch(movies_url, options)
+    .then(response => response.json())
+    .then(data => {
+        const movies = data.results;
+        let output = '';
+        let fav_btn_content = '';
+        for(let movie of movies) {
+            fav_btn_content = checkFavorite(movie.id, 'movie');
+            if (movie.overview.length > 350) {
+                movie.overview = movie.overview.substring(0,350) + "...";
+            }
+            let release_year = new Date(movie.release_date).getFullYear();
+            output += `
                 <div class="card" data-id="${movie.id}" data-type="movie">
                     <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="">
                     <div class="card_content">
                         <div class="card_title">
-                            <h3>${movie.title}</h3><p>${movie.vote_average} <i class="ti ti-star"></i> | ${movie.release_date}</p>
+                            <h3>${movie.title}</h3><p>${movie.vote_average} <i class="ti ti-star"></i> | ${release_year}</p>
                         </div>
                         <p>${movie.overview}</p>
                         <div class="card_actions">
@@ -215,24 +231,25 @@ function getAllPopular(genre = null, type = null) {
         })
         .catch(err => console.error(err));
     
-        fetch('https://api.themoviedb.org/3/tv/popular?language=fr', options)
-        .then(response => response.json())
-        .then(data => {
-            const tvshows = data.results;
-            let output = '';
-            let fav_btn_content = '';
-            for(let tvshow of tvshows) {
-                fav_btn_content = checkFavorite(tvshow.id, 'tvshow');
-                if (tvshow.overview.length > 350) {
-                    tvshow.overview = tvshow.overview.substring(0,350) + "...";
-                }
-                output += `
+    fetch(tvshows_url, options)
+    .then(response => response.json())
+    .then(data => {
+        const tvshows = data.results;
+        let output = '';
+        let fav_btn_content = '';
+        for(let tvshow of tvshows) {
+            fav_btn_content = checkFavorite(tvshow.id, 'tvshow');
+            if (tvshow.overview.length > 350) {
+                tvshow.overview = tvshow.overview.substring(0,350) + "...";
+            }
+            let release_year = new Date(tvshow.first_air_date).getFullYear();
+            output += `
                 <div class="card" data-id="${tvshow.id}" data-type="tvshow">
                     <img src="https://image.tmdb.org/t/p/w500${tvshow.poster_path}" alt="">
                     <div class="card_content">
                         <div class="card_title">
                             <h3>${tvshow.name}</h3>
-                            <p>${tvshow.vote_average} <i class="ti ti-star"></i> | ${tvshow.first_air_date}</p>
+                            <p>${tvshow.vote_average} <i class="ti ti-star"></i> | ${release_year}</p>
                         </div>
                         <p>${tvshow.overview}</p>
                         <div class="card_actions">
@@ -245,19 +262,36 @@ function getAllPopular(genre = null, type = null) {
             document.getElementById('tvshows_display').innerHTML = output;
         })
         .catch(err => console.error(err));
-    }
+}
+
+function getGenres() {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?language=fr', options)
+    .then(response => response.json())
+    .then(data => {
+        const genres = data.genres;
+        let output = '';
+        for(let genre of genres) {
+            output += `<a data-id="${genre.id}" data-type="movie" id="genre">${genre.name}</a>`;
+        }
+        genres_movies.innerHTML = output;
+    })
+    .catch(err => console.error(err));
+    
+    fetch('https://api.themoviedb.org/3/genre/tv/list?language=fr', options)
+    .then(response => response.json())
+    .then(data => {
+        const genres = data.genres;
+        let output = '';
+        for(let genre of genres) {
+            output += `<a data-id="${genre.id}" data-type="tvshow" id="genre">${genre.name}</a>`;
+        }
+        genres_tvshows.innerHTML = output;
+    })
+    .catch(err => console.error(err));
 }
 
 getAllPopular();
-
-function getGenres(type) {
-    if(type === 'movies') {
-        fetch('https://api.themoviedb.org/3/genre/movie/list?language=fr', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    }
-}
+getGenres();
 
 // 
 // FAVORITES (LOCALSTORAGE)
@@ -279,13 +313,10 @@ function resetFavorites() {
 // Return button content
 function checkFavorite(id, type) {
     let favorites = JSON.parse(localStorage.getItem('favorites'));
-    const index = favorites.findIndex(favorite => favorite.id === id && favorite.type === type);
-    console.log(index);
+    const index = favorites.findIndex(favorite => favorite.id.toString() === id.toString() && favorite.type === type);
     if(index === -1) {
-        console.log('not favorite');
         return add_to_favorites;
     } else {
-        console.log('favorite');
         return remove_to_favorites;
     }
 }
@@ -308,15 +339,160 @@ function addOrRemoveFavorite(id, type, button) {
     localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
+async function loadFavorites() {
+    let favorites = JSON.parse(localStorage.getItem('favorites'));
+    let output = '';
+    for(let favorite of favorites) {
+        if(favorite.type === 'movie') {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${favorite.id}?language=fr`, options)
+            const data = await response.json();
+            if (data.overview.length > 350) {
+                data.overview = data.overview.substring(0,350) + "...";
+            }
+            let release_year = new Date(data.release_date).getFullYear();
+            output += `
+            <div class="card" data-id="${data.id}" data-type="movie">
+                <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="">
+                <div class="card_content">
+                    <div class="card_title">
+                        <h3>${data.title}</h3><p>${data.vote_average} <i class="ti ti-star"></i> | ${release_year}</p>
+                    </div>
+                    <p>${data.overview}</p>
+                    <div class="card_actions">
+                        <a class="card_btn" id="see_details"><i class="ti ti-dots-circle-horizontal"> </i>Voir les détails</a>
+                        <a class="card_btn" id="add_favorites">${remove_to_favorites}</a>
+                    </div>
+                </div>
+            </div>`
+        } else if(favorite.type === 'tvshow') {
+            const response = await fetch(`https://api.themoviedb.org/3/tv/${favorite.id}?language=fr`, options)
+            const data = await response.json();
+            if (data.overview.length > 350) {
+                data.overview = data.overview.substring(0,350) + "...";
+            }
+            let release_year = new Date(data.first_air_date).getFullYear();
+            output += `
+            <div class="card" data-id="${data.id}" data-type="tvshow">
+                <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="">
+                <div class="card_content">
+                    <div class="card_title">
+                        <h3>${data.name}</h3>
+                        <p>${data.vote_average} <i class="ti ti-star"></i> | ${release_year}</p>
+                    </div>
+                    <p>${data.overview}</p>
+                    <div class="card_actions">
+                        <a class="card_btn" id="see_details"><i class="ti ti-dots-circle-horizontal"> </i>Voir les détails</a>
+                        <a class="card_btn" id="add_favorites">${remove_to_favorites}</a>
+                    </div>
+                </div>
+            </div>`
+        }
+    }
+    favorites_display.innerHTML = output;
+}
+
+
+// Details
+function updateModal(data) {
+    const year = new Date(data.release_date).getFullYear();
+    let duration = '';
+    if(data.type === 'movie') {
+        duration = (Math.floor(data.duration / 60)) + 'h' + (data.duration % 60);
+    } else if(data.type === 'tvshow') {
+        duration = data.duration += ' saisons';
+    }
+    const rating = (data.rating * 10).toFixed(2) + '%';
+    let genres = '';
+    for(let genre of data.genres) {
+        genres += `<span>${genre}</span>`;
+    }
+    const companies = data.production_companies.join(', ');
+
+    modal_content.innerHTML = `
+        <img src="${data.poster}" alt="">
+        <i class="close ti ti-x" id="modal_close_btn"></i>
+        <div class="modal-body">
+            <div class="modal-title">
+                <h1>${data.title}</h1>
+                <span>${year} - ${duration}</span>
+                <div class="pills">
+                    <span class="green">Recommandé à ${rating}</span>
+                    ${genres}
+                </div>
+            </div>
+            <div class="modal-description">
+                <h2>Synopsis</h2>
+                <p>${data.synopsis}</p>
+            </div>
+            <p class="modal-footer">Par ${companies}</p>
+        </div>
+    `;
+    details_modal.style.display = 'block';
+}
+
+function getDetails(id, type) {
+    if(type === 'movie') {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?language=fr`, options)
+        .then(response => response.json())
+        .then(data => {
+            const movie = {
+                type: 'movie',
+                title: data.title,
+                poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
+                release_date: data.release_date,
+                duration: data.runtime,
+                rating: data.vote_average,
+                genres: data.genres.map(genre => genre.name),
+                synopsis: data.overview,
+                production_companies: data.production_companies.map(company => company.name)
+            }
+            updateModal(movie);
+        })
+        .catch(err => console.error(err));
+    } else if(type === 'tvshow') {
+        fetch(`https://api.themoviedb.org/3/tv/${id}?language=fr`, options)
+        .then(response => response.json())
+        .then(data => {
+            const tvshow = {
+                type: 'tvshow',
+                title: data.name,
+                poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
+                release_date: data.first_air_date,
+                duration: data.number_of_seasons,
+                rating: data.vote_average,
+                genres: data.genres.map(genre => genre.name),
+                synopsis: data.overview,
+                production_companies: data.production_companies.map(company => company.name)
+            }
+            updateModal(tvshow);
+        })
+        .catch(err => console.error(err));
+    }
+}
+
 document.addEventListener('click', function(e) {
     if(e.target.id === 'see_details') {
-        console.log(e.target.closest('.card').dataset.id);
-        console.log(e.target.closest('.card').dataset.type);
+        let details_id = e.target.closest('.card').dataset.id;
+        let details_type = e.target.closest('.card').dataset.type;
+        getDetails(details_id, details_type);
     } else if(e.target.id === 'add_favorites') {
         let fav_id = e.target.closest('.card').dataset.id;
         let fav_type = e.target.closest('.card').dataset.type;
         addOrRemoveFavorite(fav_id, fav_type, e.target);
+        if(active_tab === 'favorites') {
+            const cards = favorites_display.querySelectorAll('.card');
+            for(let card of cards) {
+                if(card.dataset.id === fav_id && card.dataset.type === fav_type) {
+                    card.remove();
+                }
+            }
+            getAllPopular();
+        }
+    } else if(e.target.id === 'genre') {
+        let genre_id = e.target.dataset.id;
+        let genre_type = e.target.dataset.type;
+        getAllPopular(genre_id, genre_type);
+    } else if(e.target.id === 'modal_close_btn') {
+        details_modal.style.display = 'none';
     }
 });
-
-// localStorage.removeItem('favorites');
